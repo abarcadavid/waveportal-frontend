b562d16da89fba3016c964bad84d6a1e8262c273
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-20 20:46:31 -0700

    Check for ethereum object and set account state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
+const getEthereumObject = () => window.ethereum;
+
+const getMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    if (!ethereum) {
+      console.log("No MetaMask wallet found!")
+      return null;
+    }
+
+    console.log("We have the ethereum object: ", ethereum);
+    const accounts = await ethereum.request({method: "eth_accounts"});
+
+    if (accounts.length) {
+      console.log("We found a valid accounts in the wallet");
+      const account = accounts[0];
+      return account;
+    } else {
+      console.log("No valid accounts found");
+      return null;
+    }
+
+  } catch (error) {
+    console.log("Error: ", error);
+    return null;
+  }
+}
+
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  useEffect(() => {
+    getMetaMaskAccount().then((account) => {
+      // If there is a valid account, set the account state
+      if (account) {
+        setCurrentAccount(account);
+      }
+    });
+  }, []);
 
   const wave = () => {
     
