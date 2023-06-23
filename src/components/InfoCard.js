10e723a6426dbca75e50483a1e6376ff5b86d511
abarcadavid <davidabarca.swe@protonmail.com>
2023-06-22 21:15:17 -0700

    Add Popover to display timestamp of txn

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Card } from 'tiny-ui';
+import { Card, Typography, Popover, Button} from 'tiny-ui';
+const { Text } = Typography;
 
 export default function InfoCard({ waveArray }) {
   return waveArray.map((wave, index) => {
     return(
       <Card key={index} title='Messages Stored in the Contract'>
         <Card.Content>
-          <Card title={`From: ${wave.waver}`}>
+          <Card
+            title={`From: ${wave.waver}`}
+            extra={
+              <Popover trigger="hover" content={wave.timestamp.toString()}>
+                <Button>Timestamp</Button>
+              </Popover>
+            }>
             <Card.Content>
-              <div>Timestamp: {wave.timestamp.toString()}</div>
-              <div>Message: {wave.message}</div>
+              <Text strong>Message: </Text>
+              <Text>{wave.message}</Text>
             </Card.Content>
           </Card>
         </Card.Content>
